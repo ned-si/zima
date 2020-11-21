cdd6dbb6e871613f98f3f81553425d909be02806
nedsi <nedsi@pm.me>
2020-11-22 00:31:49 +0100

    v1

--- keymap.c
+++ keymap.c
@@ -18,53 +18,80 @@
 // Defines the keycodes used by our macros in process_record_user
 // enum custom_keycodes { QMKBEST = SAFE_RANGE, QMKURL };
 
-enum custom_keycodes {
-    HS_PCB = SAFE_RANGE,
-    HS_CASE
+uint8_t mod_state;
+
+enum layers {
+    _WIN = 0,
+    _LINUX,
+    _NUMPAD,
+    _GW2,
+    _RGB,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(/* Base */
+    [_WIN] = LAYOUT(
                  XXXXXXX,  XXXXXXX,  XXXXXXX,
                  XXXXXXX,  XXXXXXX,  XXXXXXX,
-                 HS_PCB,   HS_CASE,  XXXXXXX,
-                    KC_A,     KC_B,  XXXXXXX)
-};
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX),
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case HS_PCB:
-            if (record->event.pressed) {
-                SEND_STRING("853400");
-            } else {
-            }
-            break;
-        case HS_CASE:
-            if (record->event.pressed) {
-                SEND_STRING("847160");
-            } else {
-            }
-            break;
-    }
-    return true;
-}
+    [_LINUX] = LAYOUT(
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX),
+
+    
+    [_NUMPAD] = LAYOUT(
+                 XXXXXXX,     KC_0,   KC_DOT,
+                    KC_7,     KC_8,     KC_9,
+                    KC_4,     KC_5,     KC_6,
+                    KC_1,     KC_2,     KC_3),
+  
+    [_GW2] = LAYOUT(
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX,
+                 XXXXXXX,  XXXXXXX,  XXXXXXX),
+
+    [_RGB] = LAYOUT(
+                 XXXXXXX,  RGB_MOD, RGB_RMOD,
+                 RGB_TOG,  RGB_SAI,  RGB_SAD,
+                 XXXXXXX,  RGB_HUI,  RGB_HUD,
+                 XXXXXXX,  RGB_VAI,  RGB_VAD),
+};
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 	return OLED_ROTATION_180;
 }
 
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
+static void render_status(void) {
+  // Host Keyboard Layer Status
+  oled_write_P(PSTR("Layer: "), false);
+  switch (get_highest_layer(layer_state)) {
+      case _WIN:
+          oled_write_P(PSTR("windows\n"), false);
+          break;
+      case _LINUX:
+          oled_write_P(PSTR("linux\n"), false);
+          break;
+      case _NUMPAD:
+          oled_write_P(PSTR("numpad\n"), false);
+          break;
+      case _GW2:
+          oled_write_P(PSTR("gw2\n"), false);
+          break;
+      case _RGB:
+          oled_write_P(PSTR("rgb\n"), false);
+          break;
+      default:
+          oled_write_P(PSTR("undefined\n"), false);
+  }
 }
 
 void oled_task_user(void) {
-    render_qmk_logo();
+    render_status();
 }
 #endif
 
@@ -75,14 +102,19 @@ void matrix_scan_user(void) {}
 void led_set_user(uint8_t usb_led) {}
 
 #ifdef ENCODER_ENABLE
+uint8_t selected_layer = 0;
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) {
-        // Move whole words. Hold shift to select while moving.
-        if (clockwise) {
-            tap_code16(C(KC_RGHT));
-        } else {
-            tap_code16(C(KC_LEFT));
-        }
-    }
+   if (index == 0) {
+		switch (index) {
+			case 0:
+			if (!clockwise && selected_layer  < 6) {
+				selected_layer ++;
+			} else if (clockwise && selected_layer  > 0){
+				selected_layer --;
+			}
+			layer_clear();
+			layer_on(selected_layer);
+		}
+  }
 }
 #endif
